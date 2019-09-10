e20826912f9b22335b9f1c75fdc84c02fef3c4f1
Keenan Alves <keenan.alves@gmail.com>
2019-09-10 22:40:06 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -26,7 +26,7 @@ coupons.each do |coupon|
   if cart[item] 
     if cart[item][:count] >= coupon[:num] && !cart.has_key?("#{item} W/COUPON")
     cart["#{item} W/COUPON"] = { price: coupon[:cost] / coupon[:num] , clearance: cart[item][:clearance] , count: coupon[:num] }
-    cart[item][:count] -= coupon[:num]
+    
     cart[item][:count] -= coupon[:num]
     elsif cart[item][:count] >= coupon[:num] && cart.has_key?("#{item} W/COUPON")
       cart["#{item} W/COUPON"][:count] += coupon[:num]
@@ -52,6 +52,7 @@ def checkout(array, coupons)
   hash_cart = consolidate_cart(array)
   applied_coupons = apply_coupons(hash_cart, coupons)
   applied_discount = apply_clearance(applied_coupons)
+  
   total = applied_discount.reduce(0) { |acc, (key, value)| acc += value{:price} * value[:count]}
   total > 100 ? total * 0.9 : total 
 end
