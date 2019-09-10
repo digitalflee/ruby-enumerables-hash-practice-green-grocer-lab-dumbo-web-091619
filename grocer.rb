cb80ec6ef28b9591b062194f70beb70eeb2b7e96
Keenan Alves <keenan.alves@gmail.com>
2019-09-10 21:37:44 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -7,7 +7,7 @@ def consolidate_cart(cart)
 	  element_stats = element_hash.values[0]
 	  
 	  if final_hash.has_key?(element_name)
-	    element_stats[:count] += 1 
+	    final_hash[element_name][:count] += 1 
 	    else 
 	      final_hash[element_name] = {
 	        count: 1,
