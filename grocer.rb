10fdd00aa7c34f82b710b302600f50da54f59cd1
Keenan Alves <keenan.alves@gmail.com>
2019-09-10 22:09:07 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -29,7 +29,7 @@ coupons.each do |coupon|
     cart[item][:count] -= coupon[:num]
     elsif coupon[:item][:count] >= coupon[:num] && cart["#{item} W/COUPON"]
       cart["#{item} W/COUPON"][:count] += coupon[:num]
-      
+      cart[item][:count] -= coupon[:num]
     end 
   end
 end
