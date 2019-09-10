ee520bd1e883f70dc96e454a68537a6de2776c5e
Keenan Alves <keenan.alves@gmail.com>
2019-09-10 22:05:18 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -23,7 +23,13 @@ end
 def apply_coupons(cart, coupons)
 coupons.each do |coupon|
   item = coupon[:item]
-  if cart[item] && 
+  if cart[item] && coupon[:item][:count] >= coupon[:num] && !cart["#{item} W/COUPON"]
+    
+    cart["#{item} W/COUPON"] = { price: coupon[:cost] / coupon[:num] , clearance: cart[item][:clearance] , count: coupon[:num] }
+    
+    cart[item][:count] -= coupon[:num]
+    
+    elsif 
   
   end
 end
