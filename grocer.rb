c3f4132f99198e9b8f8f7b3c7451ba32c6667d53
Keenan Alves <keenan.alves@gmail.com>
2019-09-10 21:20:24 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -3,6 +3,12 @@
 def consolidate_cart(cart)
 	final_hash = {} 
 	cart.each do |element_hash|
+	  element_name = element_hash.keys[0]
+	  element_stats = element_hash.values[0]
+	  
+	  if 
+	    else 
+	  end 
 	end 
 end
 
