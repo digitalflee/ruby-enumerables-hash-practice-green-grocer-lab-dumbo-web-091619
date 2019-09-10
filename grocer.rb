7c133e921aeb733e904075c4a4edbfe6b8e44b43
Keenan Alves <keenan.alves@gmail.com>
2019-09-10 22:10:24 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -27,7 +27,7 @@ coupons.each do |coupon|
     cart["#{item} W/COUPON"] = { price: coupon[:cost] / coupon[:num] , clearance: cart[item][:clearance] , count: coupon[:num] }
     
     cart[item][:count] -= coupon[:num]
-    elsif coupon[:item][:count] >= coupon[:num] && cart["#{item} W/COUPON"]
+    elsif cart[:item][:count] >= coupon[:num] && cart["#{item} W/COUPON"]
       cart["#{item} W/COUPON"][:count] += coupon[:num]
       cart[item][:count] -= coupon[:num]
     end 
